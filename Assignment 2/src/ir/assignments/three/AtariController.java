2d36b653921e1060265ce089f139186b5be975b8
LindaFe <lfbenson@uci.edu>
2016-02-03 20:50:35 -0800

    Update AtariController
    created global Map allPages
    created global int counEss that counts all pages visited
    moved sortAndSave to Atari and edited it to handle subdomains properly (with count)

--- Assignment 2/src/ir/assignments/three/AtariController.java
+++ Assignment 2/src/ir/assignments/three/AtariController.java
@@ -1,12 +1,40 @@
 package src.ir.assignments.three;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class AtariController {
+	
+	public static HashMap<String, Integer> allPages = new HashMap<String, Integer>();
+	
+	public static int countEss = 0;
+	
+	public static void sortAndSave() throws UnsupportedEncodingException, FileNotFoundException, IOException {
+        Map<String, Integer> sortedMap = new TreeMap<String, Integer>(allPages);
+        
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                  new FileOutputStream("Subdomains.txt"), "utf-8"))) {
+        		writer.write("Number of Unique Subdomains: " + sortedMap.size() + "\n");
+        	for (String s : sortedMap.keySet()) {
+                writer.write(s + ", " + sortedMap.get(s).intValue() + "\n");
+        	}         
+        }
+    }
+	
 	public static void main(String[] args) throws Exception {		//basic crawler controller
 		LocalDateTime startTime = LocalDateTime.now();
 		String crawlStorageFolder = "/data/crawl/root";
-        int numberOfCrawlers = 1;
+        int numberOfCrawlers = 3;
 
         CrawlConfig config = new CrawlConfig();
         config.setCrawlStorageFolder(crawlStorageFolder);
@@ -27,13 +55,18 @@ public class AtariController {
         controller.addSeed("http://www.ics.uci.edu/~lopes/");
         controller.addSeed("http://www.ics.uci.edu/~welling/");
         controller.addSeed("http://www.ics.uci.edu/");
+        controller.addSeed("https://intranet.ics.uci.edu/");
+        controller.addSeed("http://booklist.ics.uci.edu");
+        controller.addSeed("https://booklist.ics.uci.edu/?page_id=167");
 
         /*
          * Start the crawl. This is a blocking operation, meaning that your code
          * will reach the line after this only when crawling is finished.
          */
         controller.start(MyCrawler.class, numberOfCrawlers);
+        sortAndSave();
         LocalDateTime endTime = LocalDateTime.now();
+        System.out.println("Number of unique pages crawled: " + countEss);
         System.out.println(startTime + " " + endTime );
     }
 }
